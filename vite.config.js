dbcec8a5e29753dd171cf470f28d9a79eb05473e
auzima <aurelia.zima-tukala@heig-vd.ch>
2025-02-03 10:21:28 +0100

    modifier vite.config.js pour inclure public/

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: 'src', // Définir 'src' comme racine
+  publicDir: '../public',  // Assure que `public/` est bien pris en compte
   build: {
+    copyPublicDir: true,     // Copie automatiquement `public/` vers `dist/`
     target: 'es6',
     outDir: '../dist', // Répertoire de sortie
     emptyOutDir: true, // Nettoie le dossier avant chaque build
